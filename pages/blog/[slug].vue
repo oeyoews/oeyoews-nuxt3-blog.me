120198fc917854d42daf3f2adea4edeabcd0fee1
oeyoews <jyao4783@gmail.com>
2024-02-28 21:05:12 +0800

    update

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -9,11 +9,17 @@ const { slug } = useRoute().params;
 <template>
   <article
     class="py-6 max-w-none prose prose-indigo dark:prose-invert prose-img:my-2 prose-img:rounded-md overflow-x-hidden dark:text-[#cdd9e5]">
-    <ContentDoc :path="`/${slug}`" v-slot="{ doc }">
-      <h1 class="capitalize text-balance dark:text-[#cdd9e5] text-center">
-        {{ doc.title }}
-      </h1>
-      <ContentRenderer :value="doc" />
+    <ContentDoc :path="`/${slug}`">
+      <template #not-found> nothing </template>
+      <template #empty>
+        <small>空空如也！</small>
+      </template>
+      <template #default="{ doc }">
+        <h1 class="capitalize text-balance dark:text-[#cdd9e5] text-center">
+          {{ doc.title }}
+        </h1>
+        <ContentRenderer :value="doc" />
+      </template>
     </ContentDoc>
   </article>
 </template>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,27 +1,22 @@
 <template>
   <main>
-    <ContentList
-      path="/"
-      :query="{
-        draft: false,
-        sort: [
-          {
-            date: -1,
-          },
-        ],
-      }"
-      v-slot="{ list }">
-      <div v-for="post in list" :key="post._path">
-        <NuxtLink
-          :to="`/blog${post._path}`"
-          class="no-underline"
-          v-if="post._path !== '/'">
-          <h2
-            class="flex flex-col justify-between rounded p-6 text-base my-8 dark:bg-[#2d333b] hover:shadow-sm bg-transparent border-[#d0d7de] dark:border-[#444c56] border hover:outline outline-[#d0d7de] hover:outline-1 dark:outline-[#444c56] border-solid overflow-auto">
-            {{ post.title }}
-          </h2>
-        </NuxtLink>
-      </div>
+    <ContentList path="/">
+      <template #default="{ list }">
+        <div v-for="post in list" :key="post._path">
+          <NuxtLink
+            :to="`/blog${post._path}`"
+            class="no-underline"
+            v-if="post._path !== '/'">
+            <h2
+              class="flex flex-col justify-between rounded p-6 text-base my-8 dark:bg-[#2d333b] hover:shadow-sm bg-transparent border-[#d0d7de] dark:border-[#444c56] border hover:outline outline-[#d0d7de] hover:outline-1 dark:outline-[#444c56] border-solid overflow-auto">
+              {{ post.title }}
+            </h2>
+          </NuxtLink>
+        </div>
+      </template>
+      <template #not-found>
+        <p>No articles found.</p>
+      </template>
     </ContentList>
   </main>
 </template>
